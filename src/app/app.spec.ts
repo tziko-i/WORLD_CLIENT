fe7ef4919646b273720a9f37dc92d79febcb921e
Yossi Cohen <yossi@example.com>
2025-09-30 14:50:05 +0300

    new angular

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,3 +1,6 @@
+//נוסף
+import { provideZonelessChangeDetection } from '@angular/core';
+
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 
@@ -5,7 +8,10 @@ describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-    }).compileComponents();
+      //נוסף
+    providers: [provideZonelessChangeDetection()]
+   
+  }).compileComponents();
   });
 
   it('should create the app', () => {
@@ -18,6 +24,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, client');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, WORLD_CLIENT');
   });
 });
